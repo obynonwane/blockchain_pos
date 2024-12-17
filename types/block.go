f8cae9d1318a275a3bbe4d5f399d70b54d9ebf4a
obynonwane <biostechnologyng@gmail.com>
2024-12-17 16:39:55 +0100

    added test to verify block signing

--- types/block.go
+++ types/block.go
@@ -3,9 +3,18 @@ package types
 import (
 	"crypto/sha256"
 
+	"github.com/obynonwane/blockchain_project/crypto"
+
 	"github.com/obynonwane/blockchain_project/proto"
 	pb "google.golang.org/protobuf/proto"
 )
+
+// Anytime a validator commits a block
+// He needs to sign that block(i.e hash of the block)
+// so that every other validator can validate the block
+func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
+	return pk.Sign(HashBlock(b))
+}
 
 // hashing a block means hashing only the header without the
 // transaction
